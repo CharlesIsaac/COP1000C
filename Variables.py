7deba0514591349a13294152879ac0ccc2424a7d
Charles <caisaac2013@gmail.com>
2026-06-12 23:35:00 -0400

    add Variables

--- Variables.py
+++ Variables.py
@@ -1,7 +1,32 @@
 #5 multiplied by 6
+problem=input("")
+answer=5*6
+print("The answer to your query is:",answer)
+print()
 #8 divided by 3
+problem=input("")
+answer=8/3
+print("The answer to your query is:",answer)
+print()
 #8 divided by 3 without the remainder
+problem=input("")
+answer=8//3
+print("The answer to your query is:",answer)
+print()
 #8 modulo 3 (remainder only)
+problem=input("")
+answer=8%3
+print("The answer to your query is:",answer)
+print()
 #4200000 add 23090
+problem=input("")
+answer=4200000+23090
+print("The answer to your query is:",answer)
+print()
 #4200000 subtract 7633
-
+problem=input("")
+answer=4200000-7633
+print("The answer to your query is:",answer)
+print()
+print()
+print("Charles Isaac")
