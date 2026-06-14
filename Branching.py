071082f09b93434773ff3fc8035d9f37082f259a
Charles <caisaac2013@gmail.com>
2026-06-14 17:49:42 -0400

    add Branching

--- Branching.py
+++ Branching.py
@@ -1,1 +1,11 @@
-print("Hello World")
+total=0.0
+#enter kwh used
+kwh=int(input("Enter the KW hours used:"))
+if (kwh<=1000):
+    total=kwh*0.07633
+else:
+    total = 1000 * 0.07633
+    kwh-=1000
+    total=total+(kwh*0.09259)
+
+print("Amount owed is $",total)
