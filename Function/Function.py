ce1e4de111b56d60f9b33aca83b4e6e2182989c9
Charles <caisaac2013@gmail.com>
2026-06-24 17:40:38 -0400

    add Function

---
+++ Function/Function.py
@@ -0,0 +1,14 @@
+
+
+
+def findArea(r):
+    area= 3.14 * (r^2)
+    return area
+
+def totalDue (money,tax):
+    total=money + (money * tax)
+    return total
+
+def celsius (fahrenheit ):
+    cel= (fahrenheit - 32) * (5/9)
+    return cel
