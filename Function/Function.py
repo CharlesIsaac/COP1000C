754eb4fb5e3718f946dd03f9bce89c3878cf9b05
Charles <caisaac2013@gmail.com>
2026-06-25 14:15:44 -0400

    writing function

--- Function/Function.py
+++ Function/Function.py
@@ -14,5 +14,16 @@ def celsius(fahrenheit):
     return cel
 
 print(findarea(int(input())))
+print(findarea(int(input())))
+print(findarea(int(input())))
+print(findarea(int(input())))
+print(findarea(int(input())))
+
 print(f"{totaldue(int(input()),float(input())):.2f}")
+print(f"{totaldue(int(input()),float(input())):.2f}")
+print(f"{totaldue(int(input()),float(input())):.2f}")
+
+print(int(celsius(int(input()))))
+print(f"{celsius(int(input())):.4f}")
+print(f"{celsius(int(input())):.4f}")
 print(f"{celsius(int(input())):.5f}")
